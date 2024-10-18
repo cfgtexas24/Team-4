75e1d02eedd4f954c4c5142649f94ec4d809da33
melissa-ng <melissa.j.ng-1@ou.edu>
2024-10-18 18:45:35 -0500

    inserting navigation into app

--- frontend/storm-app/src/App.jsx
+++ frontend/storm-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
+import NavigationBar from './components/NavigationBar/NavigationBar';
 
 import './App.css'
 
 function App() {
   return (
     <>
+      <NavigationBar />
       <p>App</p>       
     </>
   )
--- frontend/storm-app/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/storm-app/src/components/NavigationBar/NavigationBar.jsx
@@ -1,7 +1,18 @@
+import { Squash as Hamburger } from 'hamburger-react'
+
 function NavigationBar() {
     return(
-        <>
-        </>
+        <div className='navBar'>
+            <div className='navBarProfile'>
+                
+            </div>
+            <div className='navBarLogo'>
+                <image src='../../assets/logo.webp' alt='Storm Logo'/>
+            </div>
+            <div className='navBarMenu'>
+                <Hamburger />
+            </div>
+        </div>
     )
 }
 
