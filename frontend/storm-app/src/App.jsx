7f66fc12044acb20b8fe5c8c5800dae43cb0aa19
sachi-jh <shansalia367@gmail.com>
2024-10-18 18:22:16 -0500

    router

--- frontend/storm-app/src/App.jsx
+++ frontend/storm-app/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router components
+import DashboardPage from './pages/DashboardPage/DashboardPage';
 
 import './App.css'
 
 function App() {
   return (
     <>
-      <p>App</p>       
+      <Router>
+        <Routes>
+          <Route path="/" element={<DashboardPage/>} />
+        </Routes>
+      </Router>       
     </>
   )
 }
