3666f12954d499f723a9438b893fc889679fcf39
sachi-jh <shansalia367@gmail.com>
2024-10-18 18:44:08 -0500

    tailwind but it doesn't work

--- frontend/storm-app/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/storm-app/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,6 +1,13 @@
 function DashboardPage() {
     return(
         <>
+        <h1 className="text-red-700">Hello</h1>
+        <ul className="flex flex-row text-green-600">
+            <li>Dashboard</li>
+            <li>Profile</li>
+            <li>Settings</li>
+            <li>Logout</li>
+        </ul>
         </>
     )
 }
