d6a55bcb46196be586c452075d46a4c304be37d9
melissa-ng <melissa.j.ng-1@ou.edu>
2024-10-18 18:59:13 -0500

    successfully added

--- frontend/storm-app/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/storm-app/src/components/NavigationBar/NavigationBar.jsx
@@ -1,10 +1,13 @@
+import React from 'react'
 import { Squash as Hamburger } from 'hamburger-react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function NavigationBar() {
     return(
         <div className='navBar'>
             <div className='navBarProfile'>
-                
+                <FontAwesomeIcon icon="fa-solid fa-user" />
             </div>
             <div className='navBarLogo'>
                 <image src='../../assets/logo.webp' alt='Storm Logo'/>
