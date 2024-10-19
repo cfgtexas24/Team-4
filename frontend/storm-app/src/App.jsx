547789f5846ba8b5ec9062142bf14b8d899664e3
hfsasu <hafsaasude2@gmail.com>
2024-10-18 21:52:09 -0500

    created the main community hub

--- frontend/storm-app/src/App.jsx
+++ frontend/storm-app/src/App.jsx
@@ -3,6 +3,9 @@ import SignUpPage from './pages/SignUpPage/SignUpPage';
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router components
 import DashboardPage from './pages/DashboardPage/DashboardPage';
+import CommunityPage from './pages/CommunitiesPage/CommunityPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import './App.css'
 
@@ -13,11 +16,12 @@ function App() {
       <p>App</p>       
       <Router>
         <Routes>
-          <Route path="/" element={<DashboardPage/>} />
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/community" element={<CommunityPage />} />
         </Routes>
       </Router>       
     </>
   )
 }
 
-export default App
+export default App;
