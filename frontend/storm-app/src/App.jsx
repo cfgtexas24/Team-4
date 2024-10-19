00184e0f814a42f3b92f7530c057b8b45b0f5170
sachi-jh <shansalia367@gmail.com>
2024-10-18 21:41:11 -0500

    shelf

--- frontend/storm-app/src/App.jsx
+++ frontend/storm-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router components
 import DashboardPage from './pages/DashboardPage/DashboardPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css'
 
