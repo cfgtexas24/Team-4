f8e151d9506a64a5d134b33c48d46c5860c84629
Mostaphe Mohamud <97688286+mohamudm1@users.noreply.github.com>
2024-10-18 22:44:37 -0400

    solving a merge conflict

--- frontend/storm-app/src/App.jsx
+++ frontend/storm-app/src/App.jsx
@@ -1,39 +1,24 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-=======
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import SignInPage from './pages/SignInPage/SignInPage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router components
 import DashboardPage from './pages/DashboardPage/DashboardPage';
->>>>>>> a1712541a52728ed6c9566eadcc546a9039f5390
 
-import SignInPage from "./pages/SignInPage/SignInPage";
-import SignUpPage from "./pages/SignUpPage/SignUpPage";
-
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
-    <>
-<<<<<<< HEAD
-      <p>App</p>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element=""></Route>
-
-          <Route path="/sign-in" element={<SignInPage />}></Route>
-          <Route path="/sign-up" element={<SignUpPage />}></Route>
-        </Routes>
-      </BrowserRouter>
-=======
-      <Router>
-        <Routes>
-          <Route path="/" element={<DashboardPage/>} />
-        </Routes>
-      </Router>       
->>>>>>> a1712541a52728ed6c9566eadcc546a9039f5390
-    </>
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<p>App</p>} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        
+        {/* Dashboard Route */}
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </Router>
   );
 }
 
