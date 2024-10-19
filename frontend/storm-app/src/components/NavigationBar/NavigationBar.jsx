5682487a1927b78c7308cdcad9c33a2ff382c960
melissa-ng <melissa.j.ng-1@ou.edu>
2024-10-18 20:23:52 -0500

    profile button added

--- frontend/storm-app/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/storm-app/src/components/NavigationBar/NavigationBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Squash as Hamburger } from 'hamburger-react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Logo from "../../assets/logo.webp"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './NavigationBar.css'
 
 function NavigationBar() {
     return(
-        <div className='navBar d-flex flex-row justify-content-between align-items-center'>
+        <div className='navBar d-flex flex-row justify-content-between align-items-center p-2'>
             <div className='navBarProfile'>
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 448 512"><path fill="black" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"/></svg>
             </div>
