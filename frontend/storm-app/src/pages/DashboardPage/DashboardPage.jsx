3905e4d4731501b4eff53791447acb283e28ba6f
hfsasu <hafsaasude2@gmail.com>
2024-10-18 19:23:50 -0500

    boostrap added

--- frontend/storm-app/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/storm-app/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,15 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function DashboardPage() {
-    return(
+    return (
         <>
-        <h1 className="text-red-700">Hello</h1>
-        <ul className="flex flex-row text-green-600">
-            <li>Dashboard</li>
-            <li>Profile</li>
-            <li>Settings</li>
-            <li>Logout</li>
-        </ul>
+            <h1 className="text-danger">Hello</h1>
+            <ul className="d-flex flex-row text-success list-unstyled">
+                <li className="me-3">Dashboard</li>
+                <li className="me-3">Profile</li>
+                <li className="me-3">Settings</li>
+                <li className="me-3">hiiii</li>
+                <li className="me-3">asdfasd</li>
+                <li>Logout</li>
+            </ul>
         </>
-    )
+    );
 }
 
-export default DashboardPage; 
+export default DashboardPage;
