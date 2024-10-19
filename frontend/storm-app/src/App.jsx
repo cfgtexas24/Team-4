9553cc0dda5db6b6fca4d063cc29dd193a958f6c
sachi-jh <shansalia367@gmail.com>
2024-10-19 05:07:30 -0500

    dev working

--- frontend/storm-app/src/App.jsx
+++ frontend/storm-app/src/App.jsx
@@ -10,7 +10,6 @@ import NavigationBar from './components/NavigationBar/NavigationBar';
 
 import './App.css';
 import LandingPage from './pages/LandingPage/LandingPage';
-import NavigationBar from './components/NavigationBar/NavigationBar';
 
 function App() {
   return (
