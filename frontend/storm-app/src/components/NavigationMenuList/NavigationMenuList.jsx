d4bd623e90728b090a2345cc5e7e391c1210e106
melissa-ng <melissa.j.ng-1@ou.edu>
2024-10-19 10:52:54 -0500

    nav links styling

--- frontend/storm-app/src/components/NavigationMenuList/NavigationMenuList.jsx
+++ frontend/storm-app/src/components/NavigationMenuList/NavigationMenuList.jsx
@@ -55,7 +55,7 @@ function NavigationMenuList({isOpen, setIsOpen, isSignedIn, setIsSignedIn}) {
         <>
             <ul className={`menuList ${isOpen ? 'visible' : ''}`}>
                 <li className="nav-item" onClick={handleLandingPageClick}>Home</li>
-                <li className="nav-item" onClick={handleResourcesClick}>Resources</li>
+                <a className="link nav-item" href="https://www.stormcohs.org/resources" target="_blank" rel="noopener noreferrer">Resources</a>
                 {isSignedIn
                     ? <>
                         <li className="nav-item" onClick={handleDashboardClick}>Dashboard</li>
