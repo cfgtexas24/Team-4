0eb3efc9be7db8ef59a8a8d6983173c0f8ec3edf
Raul Hernandez <raulhernandezwork@gmail.com>
2024-10-19 00:26:46 -0500

    Added emergency button for testing

--- frontend/storm-app/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/storm-app/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../index.css';
+import EmergencyButton from '../../components/EmergencyButton/EmergencyButton';
 
 function DashboardPage() {
     return (
@@ -64,6 +65,15 @@ function DashboardPage() {
                             <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M9.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383z"/>
                         </svg>
                     </div>
+                {/* Emergency Button */}
+                <div className="container mt-4">
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-left">
+                    <EmergencyButton />
+                    </div>
+                </div>
+                </div>
+
                 </div>
             </div>            
             
